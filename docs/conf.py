cc6316374c95737dbd2233796aec410306c93d03
Ondřej Nový <onovy@debian.org>
2016-08-03 19:02:48 +0200

    Use local objects.inv for intersphinx mapping

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ latex_documents = [
     ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3.5/', None),
+    'python': ('/usr/share/doc/python3-doc/html', None),
     }
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
