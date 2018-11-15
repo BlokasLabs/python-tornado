b958d28482eaa15e0648d45df755ea3b9cad1b30
Benjamin Drung <benjamin.drung@cloud.ionos.com>
2018-11-15 16:38:09 +0100

    Convert git repository from git-dpm to gbp layout

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ latex_documents = [
     ]
 
 intersphinx_mapping = {
-    'python': ('/usr/share/doc/python3-doc/html', None),
+    'python': ('https://docs.python.org/3.5/', None),
     }
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
